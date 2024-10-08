2e547c120a8099f03e9cc5b33c9991acd90a010e
алина2345804 <alina2345nakataclovo@gmail.com>
2024-10-08 20:52:40 +0800

    first comit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances_sidenav = M.Sidenav.init(elems);
+
+  var modal = document.querySelectorAll('.modal');
+  var instances_modal = M.Modal.init(elems);
+});
+
+
+
